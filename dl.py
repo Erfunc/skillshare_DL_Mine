6d4176169ca8cc3e5c6079a9d4846b5e2da55d8f
EricFun <44915667+EricFunTrick@users.noreply.github.com>
2020-11-27 03:39:46 +0330

    new changes
    new changes

--- dl.py
+++ dl.py
@@ -1,16 +1,28 @@
-import sys, os
-from skillshare import Skillshare, splash
-from magic import cookie
+import sys
+from skillshare import Skillshare
 
 # or by class ID:
 # dl.download_course_by_class_id(189505397)
 
 def main():
-    dl = Skillshare(cookie)
+    dl = Skillshare()
     course_url = sys.argv[1]
     dl.download_course_by_url(course_url)
 
 
+def info():
+    print(r"""   
+         ____  _    _ _ _     _                          ____  _     
+        / ___|| | _(_) | |___| |__   __ _ _ __ ___      |  _ \| |    
+        \___ \| |/ / | | / __| '_ \ / _` | '__/ _ \_____| | | | |    
+         ___) |   <| | | \__ \ | | | (_| | | |  __/_____| |_| | |___ 
+        |____/|_|\_\_|_|_|___/_| |_|\__,_|_|  \___|     |____/|_____|  
+
+    """)
+
+
+
 if __name__ == "__main__":
-    splash()
+    info()
     main()
+
